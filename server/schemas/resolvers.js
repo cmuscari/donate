77fcb30f2b02e6551ee8dd2239731c933b707873
gpay <gpaytakov@gmail.com>
2022-07-05 06:39:06 -0500

    schemas updated

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const { User } = require("../models");
+const { User, Post } = require("../models");
 const { signToken } = require("../utils/auth");
 const { AuthenticationError } = require("apollo-server-express");
 
@@ -8,13 +8,27 @@ const resolvers = {
       if (context.user) {
         const userData = await User.findOne({ _id: context.user._id })
           .select("-__v -password")
-          .populate("posts")
-          .populate("comments");
+          .populate("posts");
 
         return userData;
       }
 
       throw new AuthenticationError("Not logged in");
+    },
+    users: async () => {
+      return User.find().select("-__v -password").populate("posts");
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("posts");
+    },
+    posts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Post.find(params).sort({ createdAt: -1 });
+    },
+    post: async (parent, { _id }) => {
+      return Post.findOne({ _id });
     },
   },
   Mutation: {
@@ -44,7 +58,41 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-    
+    addPost: async (parent, args, context) => {
+      if (context.user) {
+        const post = await Post.create({
+          ...args,
+          username: context.user.username,
+        });
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { posts: post._id } },
+          { new: true }
+        );
+
+        return post;
+      }
+
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    addComment: async (parent, { postId, commentText }, context) => {
+      if (context.user) {
+        const updatedPost = await Post.findOneAndUpdate(
+          { _id: postId },
+          {
+            $push: {
+              comments: { commentText, username: context.user.username },
+            },
+          },
+          { new: true, runValidators: true }
+        );
+
+        return updatedPost;
+      }
+
+      throw new AuthenticationError("You need to be logged in!");
+    },
   },
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,27 +1,52 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type User {
-        _id: ID!
-        username: String
-        email: String
-        bookCount: Int
-        savedBooks: [Book]
-    }
+  type User {
+    _id: ID!
+    username: String
+    email: String
+    posts: [Post]
+  }
 
-    type Auth {
-        token: ID!
-        user: User
-    }
+  type Post {
+    _id: ID
+    category: String
+    orgName: String
+    website: URL
+    location: String
+    description: String
+    createdAt: String
+    username: String
+    commentCount: Int
+    comments: [Comment]
+  }
 
-    type Query {
-        me: User
-    }
+  type Comment {
+    _id: ID
+    commentText: String
+    username: String
+    createdAt: String
+  }
 
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-    }
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    posts(username: String): [Thought]
+    post(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addPost(category: String!, orgName: String!, website: URL!, location: String!): Post
+    addComment(postId: ID!, commentText: String!): Post
+  }
 `;
 
 // export the typeDefs
