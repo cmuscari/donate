58e1a73bcd79d2de050fad07233570b62527d32e
MJB <matt.berti13@gmail.com>
2022-07-05 19:37:20 -0500

    added resolvers for auth

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,32 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Mutation:{
+        addUser: async(parent, args) =>{
+            const user = await User.create(args);
+            const token = signToken(user)
+
+            return{ token, user };
+        },
+        login: async(parent, { email, password }) =>{
+            const user = await User.findOne({ email });
+
+            if(!user){
+                throw new AuthenticationError('Incorrect credentials')
+            }
+            const correctPw = await user.isCorrectPassword(password);
+
+            if(!correctPw){
+                throw new AuthenticationError('Incorrect credentials')
+            }
+
+            const token = signToken(user);
+            return{ token, user };
+        }
+
+
+    }
+};
+
+module.exports = resolvers;
