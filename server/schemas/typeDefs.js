92f8abb9ba4c4cded43b27e94561f4e29e8fe26b
gpay <gpaytakov@gmail.com>
2022-07-07 12:24:36 -0500

    website verification updated

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -45,7 +45,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    addPost(postText: String!, location: String!, website: String!, orgName: String!, category: String!): Post
+    addPost(postText: String!, location: String!, website: String!, orgName: String!, category: String!, description: String): Post
     addComment(postId: ID!, commentText: String!, username: String!): Post
   }
 `;
