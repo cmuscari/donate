0a880ce9fc87e2f64631ee6b99067cd52b444f80
MJB <matt.berti13@gmail.com>
2022-07-11 14:29:15 -0500

    added front end to stripe payments feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import StripeContainer from '../components/StripeContainer';
+import { useState } from 'react';
 
 function App() {
+  // state for stripe payments
+  const [showItem, setShowItem ] = useState(false);
+
+  //TODO: Set up button styling and login for onClick
+
   return (
     <main> Our App Content Here </main>
   );
+  
 }
 
 export default App;
