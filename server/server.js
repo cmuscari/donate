f6d4089a2bf5ce5958900cbe06276a7f73433ba5
MJB <matt.berti13@gmail.com>
2022-07-11 14:26:12 -0500

    added front end components for donations

--- server/server.js
+++ server/server.js
@@ -21,8 +21,38 @@ const server = new ApolloServer({
 
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+//TODO: Fix bodyParser calls and move function if necessary
+app.post('/donate', cors(), async (req, response) =>{
+    let { amount, id } = req.body
+
+    try {
+        // async function straight from stripe api to get payment info/object
+        const payment = await stripe.paymentIntents.create({
+            amount,
+            currency: "USD",
+            description: "Donation to online charity board",
+            payment_method: id,
+            confirm: true
+        })
+        console.log("Payment", payment)
+        response.json({
+            message: "Payment successful",
+            success: true
+        })
+    } catch (error) {
+        console.log("Error", error)
+        response.json({
+            message: "Payment failed",
+            success: false
+        })
+    }
+});
+
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
