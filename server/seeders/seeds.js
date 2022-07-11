dd667e50f7832162b54e197fa82208f38a4080cd
Cortnie <cortniemuscari@gmail.com>
2022-07-10 22:31:50 -0500

    added more seed data

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -4,18 +4,57 @@ const { User, Post } = require("../models");
 db.once("open", async () => {
   // seed users
   await User.create({
-    username: "Emily",
-    email: "emily@gmail.com",
-    password: "password123",
+    username: "Malory",
+    email: "malory@gmail.com",
+    password: "password111",
   });
 
   await User.create({
-    username: "Brandon",
-    email: "brandon@gmail.com",
-    password: "password456",
+    username: "Archer",
+    email: "archer@gmail.com",
+    password: "password222",
+  });
+
+  await User.create({
+    username: "Pam",
+    email: "pam@gmail.com",
+    password: "password333",
+  });
+
+  await User.create({
+    username: "Barry",
+    email: "barry@gmail.com",
+    password: "password444",
+  });
+
+  await User.create({
+    username: "Lana",
+    email: "lana@gmail.com",
+    password: "password555",
+  });
+
+  await User.create({
+    username: "Krieger",
+    email: "krieger@gmail.com",
+    password: "password666",
+  });
+
+  await User.create({
+    username: "Cheryl",
+    email: "cheryl@gmail.com",
+    password: "password777",
+  });
+
+  await User.create({
+    username: "Len",
+    email: "len@gmail.com",
+    password: "password888",
   });
 
   console.log("users seeded");
+
+
+
 
   // seed posts
   const posts = await Post.insertMany([
@@ -26,7 +65,7 @@ db.once("open", async () => {
       location: "Austin, Texas",
       description:
         "Their mission is to drive meaningful, continuous improvement of farm animal welfare through multi-level standards development, application, and verification across the supply chain.",
-      username: "Emily",
+      username: "Malory",
     },
     {
       category: "health",
@@ -35,7 +74,7 @@ db.once("open", async () => {
       location: "New York, New York",
       description:
         "The National Multiple Sclerosis Society (NMSS) is a voluntary, non-profit, health organization dedicated to ending the devastating effects of MS by advancing the cure, prevention and treatment of multiple sclerosis and by improving the lives of affected individuals.",
-      username: "Brandon",
+      username: "Archer",
     },
     {
       category: "children",
@@ -44,7 +83,61 @@ db.once("open", async () => {
       location: "Los Angeles, California",
       description:
         "I Have a Dream FDN provides long-term support to youth living in under-resourced communities to help them reach their full potential. We promote, from an early age, values of higher ed. and career success through guaranteed financial resources, empowerment programs and more.",
-      username: "Emily",
+      username: "Pam",
+    },
+    {
+      category: "health",
+      orgName: "Recipe for Success Foundation",
+      website: "https://www.recipe4success.org/",
+      location: "Houston, Texas",
+      description:
+        "The mission of the Recipe for Success Foundation is to combat childhood obesity and encourage long-term health by altering the way children understand, appreciate and eat their food.",
+      username: "Barry",
+    },
+    {
+      category: "education",
+      orgName: "826CHI",
+      website: "https://www.826chi.org/",
+      location: "Chicago, Illinois",
+      description:
+        "826CHI is a non-profit creative writing, tutoring, and publishing center dedicated to amplifying the voices of Chicago youth.",
+      username: "Lana",
+    },
+    {
+      category: "human & civil rights",
+      orgName: "Urban League of Greater Pittsburgh",
+      website: "https://ulpgh.org/",
+      location: "Pittsburgh, Pennsylvania",
+      description:
+        "The Mission of the Urban League of Greater Pittsburgh is to enable African Americans to secure economic self-reliance, parity and power, and civil rights.",
+      username: "Krieger",
+    },
+    {
+      category: "arts & culture",
+      orgName: "The Arts Community Alliance",
+      website: "https://taca-arts.org/",
+      location: "Dallas, Texas",
+      description:
+        "TACA believes in the transformative power of the arts. We exist to nurture arts organizations and provide visionary and responsive leadership to the arts community.",
+      username: "Len",
+    },
+    {
+      category: "public service & military",
+      orgName: "Twenty Two Until None",
+      website: "https://www.22untilnone.org/",
+      location: "Phoenix, Arizona",
+      description:
+        "Ending veteran suicide one step at a time. With this epidemic running through our ranks of veterans, we need to take a stand as a team. We host many events and attend many others to help our veterans in every way possible.",
+      username: "Malory",
+    },
+    {
+      category: "health",
+      orgName: "Clean the World",
+      website: "https://cleantheworld.org/",
+      location: "Orlando, Florida",
+      description:
+        "Through the distribution of donated hygiene products to impoverished people, we prevent the millions of deaths caused by hygiene-related illnesses every day.",
+      username: "Archer",
     },
   ]);
 
