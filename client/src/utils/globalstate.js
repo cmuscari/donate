0489714b83f76a561db020e0e298d88740ac5d02
jwhitehead08 <97199704+jwhitehead08@users.noreply.github.com>
2022-07-13 14:40:08 -0500

    global state

---
+++ client/src/utils/globalstate.js
@@ -0,0 +1,20 @@
+import React, { createContext, useContext, useState } from "react";
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+const StoreProvider = ({ children }) => {
+    const [category, setCategory] = useState('all');
+    const newCategory = (value) => {
+        setCategory(value);
+    }
+
+
+  return <Provider value={{category, newCategory}}>{children}</Provider>;
+};
+
+const useStoreContext = () => {
+  return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
