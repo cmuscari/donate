a16327a4e4e86c4d1706545ad09220c206cbf2b9
gpay <gpaytakov@gmail.com>
2022-07-13 11:52:29 -0500

    initial code

--- client/src/components/Homepage/index.js
+++ client/src/components/Homepage/index.js
@@ -13,7 +13,7 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useNavigate } from 'react-router-dom';
 import Auth from '../../utils/auth';
-import testCategoryIcon from '../../images/icons/animals.png';
+import Images from '../Images'
 import { Link } from 'react-router-dom';
 import { QUERY_ORGS } from '../../utils/queries';
 import { useQuery } from '@apollo/client';
@@ -94,7 +94,7 @@ export default function Album() {
                       sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
                     >
                       <CardContent className="container-content" sx={{ flexGrow: 1 }}>
-                        <img className="category-icon" src={testCategoryIcon} />
+                        <img className="category-icon" src={Images.arts} />
                         <Typography gutterBottom variant="h5" component="h2" id="org-card-name" className="org-name">
                           {post.orgName}
                         </Typography>
