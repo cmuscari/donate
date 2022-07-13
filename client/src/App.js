618fbc679df88f7280025df3708d48609e83a788
MJB <matt.berti13@gmail.com>
2022-07-12 19:04:18 -0500

    middleware for stripe

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,11 @@ function App() {
   const [showItem, setShowItem ] = useState(false);
 
   //TODO: Set up button styling and login for onClick
-
+  //TODO: GET FRONT END CODE AND ADD BUTTON AND LOGIC IN APP RETURN
   return (
     <main> Our App Content Here </main>
   );
-  
+
 }
 
 export default App;
