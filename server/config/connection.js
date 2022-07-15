25763dc2685b6393238866790d28c95217572b39
gpay <gpaytakov@gmail.com>
2022-07-15 12:05:54 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/donate', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/donate', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // useCreateIndex: true,
