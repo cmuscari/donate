b81459a92b9fe68b8a2d1b0147f19c226dbc8815
Cortnie <cortniemuscari@gmail.com>
2022-07-15 22:52:22 -0500

    heroku deploy

--- server/server.js
+++ server/server.js
@@ -22,8 +22,8 @@ const server = new ApolloServer({
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(cors());
 
